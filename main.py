ba4cc89c68a0132136dcbbca81e759f13aa7ee3b
Just R <remixingmagelang@gmail.com>
2024-10-14 00:15:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ CARD_TYPES = {
 # Pesan welcome
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply(
+    await message.answer(
         "*Welcome!*\n\nI can search _YU-GI-OH!_ cards for you! \n\nSend /help for more information!",
         parse_mode=ParseMode.MARKDOWN
     )
@@ -32,7 +32,7 @@ async def send_welcome(message: types.Message):
 # Daftar perintah
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
-    await message.reply(
+    await message.answer(
         "List of Commands \n\n/start - Welcome message\n/about - Credits and Bot information\n/card {card name} - Replies with a picture of the card.\n/stats {card name} - Replies with information about the card.\n/price {card name} - Replies with the current lowest price on TCGPlayer.\n/artworks {card name} - Replies with all the artworks for a given card\n/draw - Replies with a random card\n\nAll card names need to be the exact name of the card.",
         parse_mode=ParseMode.MARKDOWN
     )
@@ -40,14 +40,14 @@ async def send_help(message: types.Message):
 # Tentang bot
 @dp.message_handler(commands=['about'])
 async def about(message: types.Message):
-    await message.reply(
+    await message.answer(
         "*Made by @delctrl*\n\nThe *Yu-gi-oh! API* can be found at https://db.ygoprodeck.com/api-guide/",
         parse_mode=ParseMode.MARKDOWN
     )
 
 # Penanganan kesalahan
 async def handle_error(message: types.Message):
-    await message.reply("Card not found", reply_to_message_id=message.message_id)
+    await message.answer("Card not found", reply_to_message_id=message.message_id)
 
 # Mendapatkan gambar kartu berdasarkan nama
 @dp.message_handler(lambda message: message.text.startswith("/card"))
@@ -58,7 +58,7 @@ async def get_card(message: types.Message):
     if response.status_code == 200:
         data = response.json()
         card_image_url = data[0]['card_images'][0]['image_url']
-        await message.reply_photo(card_image_url, reply_to_message_id=message.message_id)
+        await message.answer_photo(card_image_url, reply_to_message_id=message.message_id)
     else:
         await handle_error(message)
 
@@ -71,7 +71,7 @@ async def get_price(message: types.Message):
     if response.status_code == 200:
         data = response.json()
         price = data[0]['card_prices'][0]['tcgplayer_price']
-        await message.reply(f'TCGPlayer Price: ${price}', reply_to_message_id=message.message_id)
+        await message.answer(f'TCGPlayer Price: ${price}', reply_to_message_id=message.message_id)
     else:
         await handle_error(message)
 
@@ -84,7 +84,7 @@ async def get_effect(message: types.Message):
     if response.status_code == 200:
         data = response.json()
         card_info = f"Name: {data[0]['name']}\nEffect: {data[0]['desc']}"
-        await message.reply(card_info, reply_to_message_id=message.message_id)
+        await message.answer(card_info, reply_to_message_id=message.message_id)
     else:
         await handle_error(message)
 
@@ -120,7 +120,7 @@ async def get_stats(message: types.Message):
         if card.get('banlist_info') and card['banlist_info'].get('ban_tcg'):
             info += f"Banlist Status: {card['banlist_info']['ban_tcg']}\n"
         
-        await message.reply(info, reply_to_message_id=message.message_id)
+        await message.answer(info, reply_to_message_id=message.message_id)
     else:
         await handle_error(message)
 
@@ -133,7 +133,7 @@ async def get_artworks(message: types.Message):
     if response.status_code == 200:
         data = response.json()
         images = [InputMediaPhoto(media=img['image_url']) for img in data[0]['card_images']]
-        await bot.send_media_group(chat_id=message.chat.id, media=images)
+        await bot.send_media_group(chat_id=message.chat.id, media=images, reply_to_message_id=message.message_id)
     else:
         await handle_error(message)
 
@@ -145,7 +145,7 @@ async def draw_card(message: types.Message):
     if response.status_code == 200:
         card = response.json()[0]
         caption = "MONSUTA CADO!!!" if card['type'] not in [CARD_TYPES['SPELL'], CARD_TYPES['TRAP']] else ""
-        await message.reply_photo(card['card_images'][0]['image_url'], caption=caption, reply_to_message_id=message.message_id)
+        await message.answer_photo(card['card_images'][0]['image_url'], caption=caption, reply_to_message_id=message.message_id)
     else:
         await handle_error(message)
 
